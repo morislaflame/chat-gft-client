aa1699bb7dccc5c6e69160fa15b58dc50e52657b
morislaflame <andrey.uraltender@yandex.ru>
2025-11-11 21:47:24 +0500

    anim

--- src/components/CoreComponents/PageWrapper.tsx
+++ src/components/CoreComponents/PageWrapper.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { motion } from 'motion/react';
 
 interface PageWrapperProps {
     children: React.ReactNode;
@@ -7,15 +8,18 @@ interface PageWrapperProps {
 
 const PageWrapper: React.FC<PageWrapperProps> = ({ children, className = '' }) => {
     return (
-        <div 
+        <motion.div 
             className={`bg-primary-900 flex text-gray-200 font-sans overflow-hidden hide-scrollbar ios-scroll ${className}`}
             style={{ 
                 height: 'calc(100vh - 122px)',
                 marginTop: `122px`,
             }}
+            initial={{ opacity: 0, y: -20 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ duration: 0.3, ease: 'easeOut' }}
         >
             {children}
-        </div>
+        </motion.div>
     );
 };
 
